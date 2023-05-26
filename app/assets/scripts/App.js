188f78aefe7caba8d07d11b2133f179708819d28
Otavia <bedeschi.otavia@gmail.com>
2023-05-26 13:28:25 +0100

    Finished style and javascript

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
+import Modal from './modules/Modal';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 
+new Modal()
 let stickyHeader = new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
