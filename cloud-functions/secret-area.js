a907140cc1f2ea4895eaa5bc9830432e31a3b4d5
Otavia <bedeschi.otavia@gmail.com>
2023-05-29 16:52:40 +0100

    Updated cloud functions to check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,28 @@
 exports.handler = function(event, context, callback){
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the super secret area"
-    })
+    const secrectContent = `
+   <h3>Welcome To The Secrect Area</h3>
+   <p>Here we can tel you the sky is <strong>blue</strong>, and two plus two equals four</p>
+    `
+    let body
+
+    if(event.body) {
+        body = JSON.parse(event.body)
+
+    }else {
+        body = {}
+    }
+
+    if(body.password == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: secrectContent
+        })
+
+    } else {
+        callback(null, {
+            statusCode: 401
+        })
+
+    }
+    
 }
